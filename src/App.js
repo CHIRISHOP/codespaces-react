496b89ac6e1cc0c9e621173b2800aa12bb575fd3
CHIRISHOP <126655766+CHIRISHOP@users.noreply.github.com>
2023-03-28 14:34:37 +0000

    commit al

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+              Hello world!
+                  </h1>
+                    )
+                    }
 }
-
-export default App;
